7c9429caf22dc8094daa73eea7c044adc4fef3a7
GentlemanGit <ivanboltrushko@gmail.com>
2021-04-24 16:17:56 +0300

    feat: add download img & fix filters

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -6,6 +6,7 @@ document.getElementById("FS").addEventListener("click", () => {
     document.documentElement.requestFullscreen();
   }
 });
+
 /*Next Image*/
 const baseDay =
   "https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/day/";
@@ -40,7 +41,7 @@ let i = 0;
 let base = baseDay;
 const imgHtml = document.querySelector("img");
 const btnNext = document.querySelector(".btn-next");
-
+drawImage();
 function changeImage() {
   let now = new Date().getHours();
   if (now >= 6 && now < 12) {
@@ -55,13 +56,25 @@ function changeImage() {
   if (now >= 0 && now < 6) {
     base = baseNight;
   }
-  console.log(base);
   const index = i % images.length;
   const imageSrc = base + images[index];
   imgHtml.src = imageSrc;
   i++;
+  drawImage();
 }
 btnNext.addEventListener("click", changeImage);
+
+/*Upload images*/
+const fileInput = document.querySelector('input[type="file"]');
+fileInput.addEventListener("change", () => {
+  const file = fileInput.files[0];
+  const reader = new FileReader();
+  reader.onload = () => {
+    imgHtml.src = reader.result;
+    drawImage();
+  };
+  reader.readAsDataURL(file);
+});
 
 /*Filters value & change Filters */
 const filterIn = document.querySelectorAll("input");
@@ -70,11 +83,7 @@ const filterOut = document.querySelectorAll("output");
 filterIn.forEach((thisImput, i) => {
   thisImput.addEventListener("input", () => {
     filterOut[i].value = thisImput.value;
-    const size = thisImput.dataset.sizing || "";
-    document.documentElement.style.setProperty(
-      `--${thisImput.name}`,
-      thisImput.value + size
-    );
+    drawImage();
   });
 });
 /*Reset Filters*/
@@ -83,13 +92,36 @@ const btnReset = document.querySelector(".btn-reset");
 function resetFilters() {
   filterIn.forEach((thisImput, i) => {
     const resetValue = thisImput.dataset.rvalue;
-    const size = thisImput.dataset.sizing || "";
     filterOut[i].value = resetValue;
     thisImput.value = resetValue;
-    document.documentElement.style.setProperty(
-      `--${thisImput.name}`,
-      resetValue + size
-    );
+    drawImage();
   });
 }
 btnReset.addEventListener("click", resetFilters);
+
+/*Draw image*/
+const canvas = document.querySelector("canvas");
+
+function drawImage() {
+  const img = new Image();
+  img.setAttribute("crossOrigin", "anonymous");
+  img.src = imgHtml.src;
+  img.onload = function () {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    const ctx = canvas.getContext("2d");
+    const filterValue = `blur(${filterIn[0].value}px) invert(${filterIn[1].value}%) sepia(${filterIn[2].value}%) saturate(${filterIn[3].value}%) hue-rotate(${filterIn[4].value}deg)`;
+    ctx.filter = filterValue;
+    ctx.drawImage(img, 0, 0);
+  };
+}
+/*Download image*/
+const btnSave = document.querySelector(".btn-save");
+btnSave.addEventListener("click", () => {
+  const link = document.createElement("a");
+  link.download = "download.png";
+  link.href = canvas.toDataURL();
+  drawImage();
+  link.click();
+  link.delete;
+});
